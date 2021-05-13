9e90f1b9d9f8cb8b6de86e854436d8d377197f10
sourabhpython <sourabhpython.099@gmail.com>
2021-05-13 22:46:58 +0530

    updated copy

--- resources/item.py
+++ resources/item.py
@@ -1,52 +1,64 @@
-import sqlite3
-from flask_restful import Resource,reqparse
+from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
 from models.item import ItemModel
+
 class Item(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('price',
         type=float,
         required=True,
-        help = "This is field can't blank"
+        help="This field cannot be left blank!"
+    )
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
     )
 
-
     @jwt_required()
-    def get(self,name):
-        item = ItemModel.find_item_by_name(name)
+    def get(self, name):
+        item = ItemModel.find_by_name(name)
         if item:
             return item.json()
-        return {'message':'item not found'},404
+        return {'message': 'Item not found'}, 404
 
-    def post(self,name):
-        
-        item = ItemModel.find_item_by_name(name)
-        if item:
-            return {"message":"An item with name {} already exists".format(name)}
-        data=Item.parser.parse_args()
-        item = ItemModel(name,data['price'])
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+        data = Item.parser.parse_args()
+
+        item = ItemModel(name, **data)
+
         try:
-            item.insert_item()
+            item.save_to_db()
         except:
-            return {'message','An error occured'},500
-        return {'message':"Item added in db"}
+            return {"message": "An error occurred inserting the item."}, 500
 
-    def delete(self,name):
-        item = ItemModel.find_item_by_name(name)
+        return item.json(), 201
+
+    def delete(self, name):
+        item = ItemModel.find_by_name(name)
         if item:
             item.delete_from_db()
-        return {"message":"Item Deleted"}
 
-    def put(self,name):
-        data=Item.parser.parse_args()
-        item = ItemModel.find_item_by_name(name)
-        if Item is None:
-            item = ItemModel(name,data['price'])
+        return {'message': 'Item deleted'}
+
+    def put(self, name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item = ItemModel(name, **data)
         else:
             item.price = data['price']
-        item.save_to_db()
-        return item.json()        
 
-class itemlist(Resource):
+        item.save_to_db()
+
+        return item.json()
+
+
+class ItemList(Resource):
     def get(self):
-        return {'items':[item.json() for item in ItemModel.query.all]}
+        return {'items': [x.json() for x in ItemModel.query.all()]}
